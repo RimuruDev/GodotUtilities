5ed3cd2da6fcef39d936a25423264fa21473d3ce
Rimuru <Rimuru.Dev@gmail.com>
2023-09-22 19:26:24 +0400

    Update Debug.cs

--- Utilities/Debug.cs
+++ Utilities/Debug.cs
@@ -13,7 +13,33 @@ using Godot;
 
 public static class Debug
 {
+    /// <summary>
+    /// Wrapper over GD.Print(params object[] parameters);
+    /// </summary>
+    /// <example>
+    /// <code>
+    /// var a = new int[] { 1, 2, 3 };
+    /// Debug.Log("a", "b", a); // Prints ab[1, 2, 3]
+    /// </code>
+    /// </example>
+    /// <param name="parameters">Arguments that will be printed.</param>
     public static void Log(params object[] parameters) => GD.Print(parameters);
+    /// <summary>
+    /// Wrapper over GD.PushWarning(string message);
+    /// </summary>
+    /// <example>
+    /// Debug.LogWarning("test warning"); // Prints "test warning" to debugger and terminal as warning call
+    /// </example>
+    /// <param name="message">Warning message.</param>
     public static void LogWarning(string message) => GD.PushWarning(message);
+    /// <summary>
+    /// Wrapper over GD.PushError(string message);
+    /// </summary>
+    /// <example>
+    /// <code>
+    /// Debug.LogError("test error"); // Prints "test error" to debugger and terminal as error call
+    /// </code>
+    /// </example>
+    /// <param name="message">Error message.</param>
     public static void LogError(string message) => GD.PushError(message);
 }
